44f266b1251a45e3676720835f85db1b12940bdd
Bruno AMUZU <amuzuelbruno@gmail.com>
2026-01-21 14:57:23 +0100

    ajout

--- new_file.py
+++ new_file.py
@@ -1,3 +1,9 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+
 def create_index():
     
     # Ouvre (ou crée) un fichier en mode écriture
@@ -18,3 +24,6 @@ def create_index():
     return True
 
 create_index()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
