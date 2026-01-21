f8d3af6473ba58237c4bd2df42b351a118abb55c
nostyn6 <nostyn6@gmail.com>
2026-01-21 14:41:00 +0100

    Add function to create a basic HTML index file
    This function creates an HTML file named 'index.html' with a basic structure and a 'Hello World!' message.

--- new_file.py
+++ new_file.py
@@ -1,1 +1,20 @@
+def create_index():
+    
+    # Ouvre (ou crée) un fichier en mode écriture
+    with open("index.html", "w") as f:
 
+        f.write('<!DOCTYPE html>' + '\n')
+        f.write('<html lang="en">' + '\n')
+        f.write('<head>' + '\n')
+        f.write('    <meta charset="UTF-8">' + '\n')
+        f.write('    <meta name="viewport" content="width=device-width, initial-scale=1.0">' + '\n')
+        f.write('    <title>Document</title>' + '\n')
+        f.write('</head>' + '\n')
+        f.write('<body>' + '\n')
+        f.write('Hello World !' + '\n')
+        f.write('</body>' + '\n')
+        f.write('</html>' + '\n')
+
+    return True
+
+create_index()
